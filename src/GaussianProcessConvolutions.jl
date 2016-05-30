2df970ea35dbbd05f76319fffa522c20c98821da
John Best <jkbest@gmail.com>
2016-05-29 20:52:21 -0800

    Change GPC type signature to AbstractArray.
    Changed from Array. Allows contructors to take Range and LinSpace
    objects (as well as lots of others that probably aren't useful,
    like sparse types).

--- src/GaussianProcessConvolutions.jl
+++ src/GaussianProcessConvolutions.jl
@@ -26,22 +26,22 @@ include("ConvolutionKernels.jl")
 
 #----------------------------------------------------------------------------
 # Basic type
-immutable GaussianProcessConvolution
-    knot_locs::Array
-    knot_values::Array
+immutable GaussianProcessConvolution <: Any
+    knot_locs::AbstractArray
+    knot_values::AbstractArray
     dim::Integer
     nknot::Integer
-    GaussianProcessConvolution(knot_locs::Array, knot_values::Vector) =
+    GaussianProcessConvolution(knot_locs::AbstractArray, knot_values::Vector) =
       new(knot_locs,
           knot_values,
           size(knot_locs, 2),
           size(knot_locs, 1))
-    GaussianProcessConvolution(knot_locs::Array, dist::UnivariateDistribution) =
+    GaussianProcessConvolution(knot_locs::AbstractArray, dist::UnivariateDistribution) =
       new(knot_locs,
           rand(dist, size(knot_locs, 1)),
           size(knot_locs, 2),
           size(knot_locs, 1))
-    GaussianProcessConvolution(knot_locs::Array) =
+    GaussianProcessConvolution(knot_locs::AbstractArray) =
       GaussianProcessConvolution(knot_locs, Normal(0, 1))
 end
 
@@ -54,7 +54,7 @@ dim(GPC::GaussianProcessConvolution) = GPC.dim
 # Putting them together
 function predict(GPC::GaussianProcessConvolution,
                  kern::AbstractConvolutionKernel,
-                 new_loc::Array)
+                 new_loc::AbstractArray)
 
     nnew = size(new_loc, 1)
     new_val = zeros(nnew)
@@ -67,14 +67,14 @@ function predict(GPC::GaussianProcessConvolution,
 end
 
 function predict(GPC::GaussianProcessConvolution,
-                 knot_wt::Array)
+                 knot_wt::AbstractArray)
     knot_wt * GPC.knot_values
 end
 
 ## Calculate knot weight matrix
 function knot_wt(GPC::GaussianProcessConvolution,
                  kern::AbstractConvolutionKernel,
-                 new_locs::Array{Float64})
+                 new_locs::AbstractArray)
     nloc = size(new_locs, 1)
     nk = nknot(GPC)
 
