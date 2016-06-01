d948771afaabd9558f785ec579020e6e634cbdf0
John Best <jkbest@gmail.com>
2016-05-31 16:49:33 -0800

    Rename GaussianProcessConvolution ProcessConvolution.
    Shorter and more accurate. A process convolution represent a
    Gaussian process if the the knots are normally distributed
    (i.e. if the knot values have a Normal prior, the convolution
    of those knots is a Gaussian process prior).

--- src/GaussianProcessConvolutions.jl
+++ src/GaussianProcessConvolutions.jl
@@ -11,7 +11,7 @@ export
     AbstractConvolutionKernel,
     SquaredExponentialKernel,
     # Basic type
-    GaussianProcessConvolution,
+    ProcessConvolution,
     GaussianProcessSample,
     # Methods
     dim,		    # get dimensionality of process or kernel
@@ -26,20 +26,20 @@ include("ConvolutionKernels.jl")
 
 #----------------------------------------------------------------------------
 # Basic type
-abstract AbstractProcessConvolution <: Any
+abstract AbstractProcess <: Any
 
-immutable GaussianProcessConvolution <: Any
+immutable ProcessConvolution <: AbstractProcess
     knot_locs::AbstractArray
-    knot_values::AbstractArray
+    knot_values::Vector
     dim::Integer
     nknot::Integer
 
-    GaussianProcessConvolution(knot_locs::AbstractArray, knot_values::Vector) =
+    ProcessConvolution(knot_locs::AbstractArray, knot_values::Vector) =
       new(knot_locs,
           knot_values,
           size(knot_locs, 2),
           size(knot_locs, 1))
-    GaussianProcessConvolution(knot_locs::AbstractArray, dist::UnivariateDistribution) =
+    ProcessConvolution(knot_locs::AbstractArray, dist::UnivariateDistribution) =
       new(knot_locs,
           rand(dist, size(knot_locs, 1)),
           size(knot_locs, 2),
@@ -57,7 +57,7 @@ dim(gpc::GaussianProcessConvolution) = gpc.dim
 
 #------------------------------------------------------------------------------
 # Putting them together
-function predict(GPC::GaussianProcessConvolution,
+function predict(pc::ProcessConvolution,
                  kern::AbstractConvolutionKernel,
                  new_loc::AbstractArray)
 
@@ -65,27 +65,27 @@ function predict(GPC::GaussianProcessConvolution,
     new_val = zeros(nnew)
 
     for l in 1:nnew
-      d = GPC.knot_locs' .- new_loc'[:, l]
-      new_val[l] = dot(conv_wt(kern, d), GPC.knot_values)
+      d = pc.knot_locs' .- new_loc'[:, l]
+      new_val[l] = dot(conv_wt(kern, d), pc.knot_values)
     end
     new_val
 end
 
-function predict(GPC::GaussianProcessConvolution,
+function predict(pc::ProcessConvolution,
                  knot_wt::AbstractArray)
-    knot_wt * GPC.knot_values
+    knot_wt * pc.knot_values
 end
 
 ## Calculate knot weight matrix
-function knot_wt(GPC::GaussianProcessConvolution,
+function knot_wt(pc::ProcessConvolution,
                  kern::AbstractConvolutionKernel,
                  new_locs::AbstractArray)
     nloc = size(new_locs, 1)
-    nk = nknot(GPC)
+    nk = nknot(pc)
 
     k_wt = Array{Float64, 2}(nloc, nk)
     for l in 1:nloc
-        k_wt[l, :] = conv_wt(kern, knot_locs(GPC)' .- new_locs[l, :]')'
+        k_wt[l, :] = conv_wt(kern, knot_locs(pc)' .- new_locs[l, :]')'
     end
     k_wt
 end
